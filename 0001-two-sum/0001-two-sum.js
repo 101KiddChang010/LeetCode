f63190dfebbe687a29c6293af875208537dbe071
Kidd <91109377+101KiddChang010@users.noreply.github.com>
2023-02-22 00:22:19 -0500

    Time: 244 ms (8.19%), Space: 42.4 MB (59.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,19 +1,14 @@
 var twoSum = function(nums, target) {
+    let result = [];
+
     for (let x = 0; x < nums.length; x++) {
-        for (let y = 1; y < nums.length; y++) {
-            if (nums[x] != nums[y] && nums[x]+nums[y] == target) {                      
-                if (nums.indexOf(nums[x]) != nums.indexOf(nums[y])) 
-                    return [nums.indexOf(nums[x]), nums.indexOf(nums[y])];
-
-                const one = nums.indexOf(nums[x]);
-                nums[one] = 0;
-                const two = nums.indexOf(nums[y]);
-                return [one, two];
+        for (let y = 1+x; y < nums.length; y++) {
+            if (nums[x] + nums[y] === target) {
+                result.push(nums.indexOf(nums[x]));
+                result.push(nums.indexOf(nums[y], y));
             }
-
-            if (nums[x] === nums[y] && nums[x]+nums[y] == target) 
-                    if (x !== y) 
-                        return [nums.indexOf(nums[x]), y];
         }
     }
-};
+
+    return result;
+}
